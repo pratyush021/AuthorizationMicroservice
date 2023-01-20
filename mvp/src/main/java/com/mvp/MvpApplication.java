93ed2e4a694078009b49127f6ab8e7989748b94c
Pratyush Ranjan Bohidar <probohidar2016@gmail.com>
2023-01-20 09:50:27 +0530

    token

--- mvp/src/main/java/com/mvp/MvpApplication.java
+++ mvp/src/main/java/com/mvp/MvpApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MvpApplication {
 
 	public static void main(String[] args) {
-
+		//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
 		SpringApplication.run(MvpApplication.class, args);
 		System.out.println("mvp 1.0v");
 	}
